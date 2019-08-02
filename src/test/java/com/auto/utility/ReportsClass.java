c4fdf8f313937736479ac0cb2a6b16eb7e1220e4
Bhargav Sathwara <bhargavsathwara94@gmail.com>
2019-08-02 16:38:25 +0530

    Add ReportsClass.java

---
+++ src/test/java/com/auto/utility/ReportsClass.java
@@ -0,0 +1,35 @@
+package com.auto.utility;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class ReportsClass {
+
+	public static ExtentTest test;
+	public static ExtentReports extent;
+	static ReadPropertyConfig readPro = new ReadPropertyConfig();
+
+	public static void startUp() {
+		reportStartUp();
+	}
+
+	public static void startTest(String testName) {
+		test = extent.createTest(testName);
+	}
+
+	public static void logStat(Status status, String message) {
+		test.log(status, message);
+	}
+
+	public static void endTest() {
+		extent.flush();
+	}
+
+	public static void reportStartUp() {
+		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(readPro.getExtentReportPath());
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+	}
+}
